e95099c5e029538a60b87793fbf504ca21301b72
Sumez <morten@eternal.dk>
2024-08-29 12:37:49 +0200

    zip patcher program as part of publish

--- patcher/publish.bat
+++ patcher/publish.bat
@@ -1,1 +1,3 @@
 dotnet publish --configuration Release -r win-x64 -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:ReadyToRun=true -p:PublishTrimmed=true -p:CopyOutputSymbolsToPublishDirectory=false --self-contained true
+cd bin\Release\net6.0\win-x64\publish
+7z a -tzip ..\..\..\..\bsdj-patcher.zip Patcher.exe
